7e6e4534b91aa59f7dbeeac39b0c3dfb295a2522
Ryan Pratama <87846980+brorp@users.noreply.github.com>
2024-03-05 01:01:21 +0700

    fix env

--- helpers/hash.js
+++ helpers/hash.js
@@ -1,11 +1,8 @@
 const crypto = require('crypto');
-// const comparePassword = (password, hashedPass) => {
-//     return bcrypt.compareSync(password, hashedPass)
-// }
+const secretCrypto = process.env.CRYPTO_SECRET
 
 const hash_password = (password) => {
-    const secret = process.env.CRYPTO_SECRET
-    const hash =  crypto.createHmac('sha256', process.env.CRYPTO_SECRET)
+    const hash =  crypto.createHmac('sha256', secretCrypto)
                         .update(password)
                         .digest('hex')
     return hash
